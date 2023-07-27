a90726dc8c0b47fb8811e8a303582273ec6fbc90
Dell <Dell@DESKTOP-92POJK7>
2023-07-27 15:57:28 +0530

    commit

---
+++ C2TC/src/com/cg/inheritance/singleInheritance.java
@@ -0,0 +1,8 @@
+package com.cg.inheritance;
+
+public class singleInheritance {
+	void add() {
+		System.out.println("hello");
+	}
+
+}
---
+++ C2TC/src/com/cg/inheritance/singleMainClass.java
@@ -0,0 +1,15 @@
+package com.cg.inheritance;
+
+public class singleMainClass extends singleInheritance {
+	void mul() {
+		System.out.print("hi");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		singleMainClass sc=new singleMainClass();
+		sc.add();
+		sc.mul();
+
+	}
+
+}
