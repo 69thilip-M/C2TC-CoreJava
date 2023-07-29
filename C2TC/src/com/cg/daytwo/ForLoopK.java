4d92d248a921096d89c344dc16c46110fe7117b9
Dell <Dell@192.168.2.6>
2023-07-29 20:56:43 +0530

    commit

---
+++ C2TC/src/com/cg/daytwo/ForLoopK.java
@@ -0,0 +1,16 @@
+package com.cg.daytwo;
+
+public class ForLoopK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {1,3,6,8,9};
+		
+		for(int i:a) {
+			System.out.println(i);
+		}
+
+
+	}
+
+}
