c295e6a7a79ec203c027a44fd33c45eb936e6aa9
Dell <Dell@192.168.2.6>
2023-07-29 21:43:21 +0530

    commit

---
+++ C2TC/src/com/cg/Static/MainMethod.java
@@ -0,0 +1,13 @@
+package com.cg.Static;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		StaticMethod m = new StaticMethod();
+		m.oo();
+
+	}
+
+}
