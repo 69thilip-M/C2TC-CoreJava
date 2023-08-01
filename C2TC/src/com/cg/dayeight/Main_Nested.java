ecbf9c9fab8b4f6f9fae99b914948ee91385f421
69thilip-M <thilipmuruganprema649@gmail.com>
2023-08-01 12:44:20 +0530

    commit

---
+++ C2TC/src/com/cg/dayeight/Main_Nested.java
@@ -0,0 +1,15 @@
+package com.cg.dayeight;
+
+public class Main_Nested {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Nested_Class c1=new Nested_Class();
+		Nested_Class.class1 c2=c1.new class1();
+		c1.run();
+		c2.walk();
+		
+
+	}
+
+}
---
+++ C2TC/src/com/cg/dayeight/Nested_Class.java
@@ -0,0 +1,13 @@
+package com.cg.dayeight;
+
+public class Nested_Class {
+	void run() {
+		System.out.println("this is class 1");
+	}
+	class class1{
+		void walk() {
+			System.out.println("this is class 2");
+		}
+	}
+
+}
